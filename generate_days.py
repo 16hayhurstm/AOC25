a43afa6b1b7171d54c805dde22d570c4bca5cee4
Myles Patrick Hayhurst <16hayhurstm@gmail.com>
2025-12-02 23:36:30 +0000

    Initial Advent of Code 2025 structure

--- generate_days.py
+++ generate_days.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-for day in range(1, 26):
+for day in range(1, 12):
     directory_name = f"day{day:02}"
     if not os.path.exists(directory_name):
         os.makedirs(directory_name)
